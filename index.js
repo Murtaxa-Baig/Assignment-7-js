96ed2ffd1d799ea79a3762dac3f943b82d699929
Murtaxa-Baig <darvaish49@gmail.com>
2023-03-28 23:23:25 +0500

    next work

--- index.js
+++ index.js
@@ -184,3 +184,475 @@ function chracterCheck() {
         alert("This character is not alphabet")
     }
 }
+
+
+function vowelCheck() {
+    let char = prompt("Enter a chracter")
+    if (char == "a" || char == "A") {
+        alert("This chracter is vowel")
+    }
+    else if (char == "e" || char == "E") {
+        alert("This chracter is vowel")
+    }
+    else if (char == "i" || char == "I") {
+        alert("This chracter is vowel")
+    }
+    else if (char == "o" || char == "O") {
+        alert("This chracter is vowel")
+    }
+    else if (char == "u" || char == "U") {
+        alert("This chracter is vowel")
+    }
+    else {
+        alert("This chracter is not vowel")
+    }
+}
+
+
+
+
+
+
+function charCheck() {
+    let num = prompt("Enter chracter")
+    if (num == "a" || num == "A") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "b" || num == "B") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "c" || num == "C") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "d" || num == "D") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "e" || num == "E") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "f" || num == "F") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "g" || num == "G") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "h" || num == "H") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "i" || num == "I") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "j" || num == "J") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "k" || num == "K") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "l" || num == "L") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "m" || num == "M") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "n" || num == "N") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "o" || num == "O") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "p" || num == "P") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "q" || num == "Q") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "r" || num == "R") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "s" || num == "S") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "t" || num == "T") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "u" || num == "U") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "v" || num == "V") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "w" || num == "W") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "x" || num == "X") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "y" || num == "Y") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == "z" || num == "Z") {
+        alert("This chracter is alphabet")
+    }
+    else if (num == 0) {
+        alert("This chracter is digit")
+    }
+    else if (num == 1) {
+        alert("This chracter is digit")
+    }
+    else if (num == 2) {
+        alert("This chracter is digit")
+    }
+    else if (num == 3) {
+        alert("This chracter is digit")
+    }
+    else if (num == 4) {
+        alert("This chracter is digit")
+    }
+    else if (num == 5) {
+        alert("This chracter is digit")
+    }
+    else if (num == 6) {
+        alert("This chracter is digit")
+    }
+    else if (num == 7) {
+        alert("This chracter is digit")
+    }
+    else if (num == 8) {
+        alert("This chracter is digit")
+    }
+    else if (num == 9) {
+        alert("This chracter is digit")
+    }
+    else {
+        alert("This is special character")
+    }
+}
+
+
+
+function upperLowerCase() {
+    let num = prompt("Enter chracter")
+    if (num == "a" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "b" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "c" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "d" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "e" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "f" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "g" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "h" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "i" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "j" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "k" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "l" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "m" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "n" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "o" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "p" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "q" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "r" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "s" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "t" ) {
+        alert("")
+    }
+    else if (num == "u" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "v" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "w" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "x" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "y" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "z" ) {
+        alert("This chracter is in lowercase")
+    }
+    else if (num == "A") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "B") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "C") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "D") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "E") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "F") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "G") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "H") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "I") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "J") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "K") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "L") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "M") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "N") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "O") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "P") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "Q") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "R") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "S") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "T") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "U") {
+        alert("This chracter is in Uppercase")
+    }
+    else if ( num == "V") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "W") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "X") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "Y") {
+        alert("This chracter is in Uppercase")
+    }
+    else if (num == "Z") {
+        alert("This chracter is in Uppercase")
+    }
+
+    else {
+        alert("This character is not alphabet")
+    }
+}
+
+
+
+function weekPrint(){
+    let num =+ prompt("Input day number")
+    if(num == 1){
+        alert("Monday")
+    }
+    else if(num == 2){
+        alert("Tuesday")
+    }
+    else if(num ==3 ){
+        alert("Wednesday")
+    }
+    else if(num ==4 ){
+        alert("Thursday")
+    }
+    else if(num == 5){
+        alert("Friday")
+    }
+    else if(num ==6 ){
+        alert("Saturday")
+    }
+    else if(num == 7){
+        alert("sunday")
+    }
+    else {
+        alert("Enetr number between 1 to 7")
+    }
+}
+
+
+
+function monthCheck(){
+    let num =+ prompt("Input day number")
+    if(num == 1){
+        alert("This month has 31 day")
+    }
+    else if(num == 2){
+        alert("This month has 28 day")
+    }
+    else if(num ==3 ){
+        alert("This month has 31 day")
+    }
+    else if(num ==4 ){
+        alert("This month has 30 day")
+    }
+    else if(num == 5){
+        alert("This month has 31 day")
+    }
+    else if(num ==6 ){
+        alert("This month has 30 day")
+    }
+    else if(num == 7){
+        alert("This month has 31 day")
+    }
+    else if(num == 8){
+        alert("This month has 31 day")
+    }
+    else if(num == 9){
+        alert("This month has 30 day")
+    }
+    else if(num == 10){
+        alert("This month has 31 day")
+    }
+    else if(num == 11){
+        alert("This month has 30 day")
+    }
+    else if(num == 12){
+        alert("This month has 31 day")
+    }
+    else {
+        alert("Enetr number between 1 to 12")
+    }
+}
+
+
+
+function noteCheck(){
+
+}
+    
+
+
+function angleCheck(){
+    let angle1 =+ prompt("Enter first angle")
+    let angle2 =+ prompt("Enter second angle")
+    let angle3 =+ prompt("Enter third angle")
+    let sum = angle1 +angle2 +angle3
+
+    if(sum==180){
+        alert("Triangle is valid")
+
+    }
+    else{
+        alert("Triangle is invalid")
+    }
+
+}
+
+
+
+function triangleCheck(){
+    let side1 =+ prompt("Enter perpendicular side")
+    let side2 =+ prompt("Enter base side")
+    let side3 =+ prompt("Enter hypotenuse side")
+    let per =side1*side1
+    let base =side2*side2           
+    let hyp =side3*side3
+    if(per + base == hyp){
+        alert("Triangle is valid")
+
+    }
+    else{
+        alert("Triangle is invalid")
+    }
+
+}
+
+
+function triangle(){
+    let angle1 =+ prompt("Enter first angle")
+    let angle2 =+ prompt("Enter second angle")
+    let angle3 =+ prompt("Enter third angle")
+
+    if(angle1==angle2 && angle1==angle3){
+        alert("This is equilateral triangle")  
+
+    }
+    else if(angle1==angle2 || angle2==angle3 || angle1==angle3){
+        alert("This is isosceles triangle")
+    }
+    else {
+        alert("This is scalene triangle")
+    }
+}
+
+
+
+
+
+
+function quadratic(){
+
+}
+
+
+
+
+function profitLoss(){
+    let orignalPrice = + prompt("Enter original price of product")
+    let sellPrice = + prompt("Enter sell price of product")
+    let profitLoss = sellPrice - orignalPrice
+    if(profitLoss > 0){
+        alert("You have profit ")
+    }
+    else if(profitLoss < 0){
+        alert("You have loss")
+    }
+    else{
+        alert("You are neither benefited nor harmed")
+    }
+}
